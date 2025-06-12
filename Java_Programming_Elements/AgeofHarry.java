60665d2c089424dab174d46dc93359aa8aa28066
Anurag Tripathi <113921508+anurag24-26@users.noreply.github.com>
2025-06-12 09:31:55 +0530

    core-java: add AgeofHarry program

---
+++ Java_Programming_Elements/AgeofHarry.java
@@ -0,0 +1,13 @@
+package Java_Programming_Elements;
+
+public class AgeofHarry {
+    public static void main(String[] args) {
+        String name="Harry";
+        int birth_year=2000;
+        int current_year=2024;
+        //Calculate age
+        int age = current_year - birth_year;
+        //Display the result
+        System.out.println(name + " is " + age + " years old.");
+    }
+}
