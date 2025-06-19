529c9eeb4f44e1ef726dd6d10304549aae511004
Anurag Tripathi <113921508+anurag24-26@users.noreply.github.com>
2025-06-19 11:07:16 +0530

    new Feat

---
+++ java_strings/StringExceptionDemo.java
@@ -0,0 +1,34 @@
+package Java_Strings;
+import java.util.Scanner;
+
+public class StringExceptionDemo {
+
+    // Method that throws the exception
+    public static void generateException(String str) {
+        System.out.println("Accessing charAt out of bounds...");
+        // This will throw exception
+        System.out.println(str.charAt(str.length()));
+    }
+
+    // Method that handles the exception
+    public static void handleException(String str) {
+        try {
+            System.out.println("Handling exception using try-catch...");
+            System.out.println(str.charAt(str.length()));
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Exception caught: " + e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.next();
+
+        // This will crash (comment after checking once)
+        // generateException(input);  
+
+        // This will handle
+        handleException(input);
+    }
+}
