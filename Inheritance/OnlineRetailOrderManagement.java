ce76cb57d9ee4c192e4007d51e86b710027211df
Anurag Tripathi <113921508+anurag24-26@users.noreply.github.com>
2025-07-02 11:43:10 +0530

    [Anurag]New Directory of inheritance with code

---
+++ Inheritance/OnlineRetailOrderManagement.java
@@ -0,0 +1,43 @@
+package Inheritance;
+
+class Order {
+    int orderId;
+    String orderDate;
+
+    Order(int orderId, String orderDate) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+    }
+
+    String getOrderStatus() {
+        return "Order placed";
+    }
+}
+
+class ShippedOrder extends Order {
+    String trackingNumber;
+
+    ShippedOrder(int orderId, String orderDate, String trackingNumber) {
+        super(orderId, orderDate);
+        this.trackingNumber = trackingNumber;
+    }
+
+    @Override
+    String getOrderStatus() {
+        return "Order shipped with tracking number: " + trackingNumber;
+    }
+}
+
+class DeliveredOrder extends ShippedOrder {
+    String deliveryDate;
+
+    DeliveredOrder(int orderId, String orderDate, String trackingNumber, String deliveryDate) {
+        super(orderId, orderDate, trackingNumber);
+        this.deliveryDate = deliveryDate;
+    }
+
+    @Override
+    String getOrderStatus() {
+        return "Order delivered on: " + deliveryDate;
+    }
+}
---
+++ Inheritance/RestaurantManagementHybrid.java
@@ -0,0 +1,35 @@
+package Inheritance;
+
+interface Worker {
+    void performDuties();
+}
+
+class Person {
+    String name;
+    int id;
+
+    Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
+
+class Chef extends Person implements Worker {
+    Chef(String name, int id) {
+        super(name, id);
+    }
+
+    public void performDuties() {
+        System.out.println("Chef is cooking meals.");
+    }
+}
+
+class Waiter extends Person implements Worker {
+    Waiter(String name, int id) {
+        super(name, id);
+    }
+
+    public void performDuties() {
+        System.out.println("Waiter is serving food.");
+    }
+}
