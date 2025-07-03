3ade0b344b23cfc097d14ca1c07404444b501fa8
Anurag Tripathi <113921508+anurag24-26@users.noreply.github.com>
2025-07-03 22:32:16 +0530

    [Anurag]Encapsulatin and Polymorphism practice

---
+++ Encapsulation_Polymorphism_Interface/1_EmployeeManagementSystem.java
@@ -0,0 +1,75 @@
+package Encapsulation_Polymorphism_Interface;
+abstract class Employee {
+    private int employeeId;
+    private String name;
+    private double baseSalary;
+
+    public Employee(int employeeId, String name, double baseSalary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    public int getEmployeeId() { return employeeId; }
+    public String getName() { return name; }
+    public double getBaseSalary() { return baseSalary; }
+
+    public void setBaseSalary(double baseSalary) {
+        if (baseSalary > 0) this.baseSalary = baseSalary;
+    }
+
+    public abstract double calculateSalary();
+
+    public void displayDetails() {
+        System.out.println("ID: " + employeeId + ", Name: " + name + ", Base Salary: " + baseSalary);
+    }
+}
+
+interface Department {
+    void assignDepartment(String deptName);
+    String getDepartmentDetails();
+}
+
+class FullTimeEmployee extends Employee implements Department {
+    private String department;
+
+    public FullTimeEmployee(int id, String name, double salary) {
+        super(id, name, salary);
+    }
+
+    public double calculateSalary() {
+        return getBaseSalary();
+    }
+
+    public void assignDepartment(String deptName) {
+        this.department = deptName;
+    }
+
+    public String getDepartmentDetails() {
+        return department;
+    }
+}
+
+class PartTimeEmployee extends Employee implements Department {
+    private int hoursWorked;
+    private double hourlyRate;
+    private String department;
+
+    public PartTimeEmployee(int id, String name, int hours, double rate) {
+        super(id, name, 0);
+        this.hoursWorked = hours;
+        this.hourlyRate = rate;
+    }
+
+    public double calculateSalary() {
+        return hoursWorked * hourlyRate;
+    }
+
+    public void assignDepartment(String deptName) {
+        this.department = deptName;
+    }
+
+    public String getDepartmentDetails() {
+        return department;
+    }
+}
---
+++ Encapsulation_Polymorphism_Interface/2_ECommercePlatform.java
@@ -0,0 +1,45 @@
+package Encapsulation_Polymorphism_Interface;
+abstract class Product {
+    private String productId;
+    private String name;
+    private double price;
+
+    public Product(String productId, String name, double price) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getProductId() { return productId; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+
+    public void setPrice(double price) {
+        if (price > 0) this.price = price;
+    }
+
+    public abstract double calculateDiscount();
+}
+
+interface Taxable {
+    double calculateTax();
+    String getTaxDetails();
+}
+
+class Electronics extends Product implements Taxable {
+    public Electronics(String id, String name, double price) {
+        super(id, name, price);
+    }
+
+    public double calculateDiscount() {
+        return getPrice() * 0.1;
+    }
+
+    public double calculateTax() {
+        return getPrice() * 0.18;
+    }
+
+    public String getTaxDetails() {
+        return "Electronics Tax: 18%";
+    }
+}
